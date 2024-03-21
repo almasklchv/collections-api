d02eb33e2bcf260f02bd649c3f3c339667e91cc8
Almas Kilichov <itsmywaytoamerica@gmail.com>
2024-03-21 19:04:22 +0500

    feat: allow cors for everyone

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
